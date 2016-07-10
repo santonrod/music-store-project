90b61bc0ff2d4dfa6c94f078c7dc5970c97734fb
Stefan <santonrod@gmail.com>
2016-07-09 19:39:05 -0500

    "update routes"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :pages
   root to: 'pages#index'
   devise_for :models
   resources :equipment
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708202734) do
+ActiveRecord::Schema.define(version: 20160710003102) do
 
   create_table "instruments", force: :cascade do |t|
     t.string   "category"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 20160708202734) do
   create_table "owners", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
