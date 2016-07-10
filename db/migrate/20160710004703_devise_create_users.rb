0a7aaaef04aa4ab83e289da9be68c9e6468caa0c
Stefan <santonrod@gmail.com>
2016-07-09 22:31:01 -0500

    update migration with name and owner

--- db/migrate/20160710004703_devise_create_users.rb
+++ db/migrate/20160710004703_devise_create_users.rb
@@ -2,10 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :name,               null: false,
-        default: ""
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :owner,                          default: false 
 
       ## Recoverable
       t.string   :reset_password_token
