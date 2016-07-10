fd43e0858605330105ba565584a434a07df6babf
Stefan <santonrod@gmail.com>
2016-07-10 00:01:03 -0500

    successfully add name field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,89 +1,37 @@
 class UsersController < Devise::RegistrationsController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+   before_action :configure_permitted_parameters, only: [:create]
+  before_filter :configure_account_update_params, only: [:update]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+   super
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  # GET /resource/edit
+  def edit
+    super
+  end
+
+  # PUT /resource
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    super
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # DELETE /resource
+   def destroy
+       super
+   end
 
  protected
 
-def configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:name, :owner, :email, :password, :password_confirmation, :avatar, :avatar_cache)
     end
-end
+  end
 
-def configure_account_update_params
+  def configure_account_update_params
     devise_parameter_sanitizer.for(:account_update)do |u|
       u.permit(:name, :owner, :email, :password, :password_confirmation, :current_password, :avatar, :avatar_cache)
+    end
   end
-end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :owner, :email, :password)
-    end
 end
