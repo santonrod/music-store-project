65909f5b4b6fa8f48649827cc7c630680b1068da
Stefan <santonrod@gmail.com>
2016-07-13 15:41:04 -0500

    fix sanitation log_in log_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
 
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_in)        << :name
+    devise_parameter_sanitizer.for(:sign_up)        << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
